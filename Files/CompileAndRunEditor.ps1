1f71b024e68c8ae49cafd08444a237eefb6f8387
Michael Delva <michael.delva@fishingcactus.com>
2026-02-10 16:20:40 +0100

    Updated template CompileAndRunEditor to allow powershell to show errors and not close silently

--- Files/CompileAndRunEditor.ps1
+++ Files/CompileAndRunEditor.ps1
@@ -1,8 +1,16 @@
-try {
-    & "$pythonScriptsFolder/ue-close-editor.exe"
-    & "$pythonScriptsFolder/ue-compile-editor.exe"
-    & "$pythonScriptsFolder/ue-run-editor.exe"
-}
-catch {
-    Write-Error $_.Exception.Message
+$commands = @(
+    "$pythonScriptsFolder/ue-close-editor.exe",
+    "$pythonScriptsFolder/ue-compile-editor.exe",
+    "$pythonScriptsFolder/ue-run-editor.exe"
+)
+
+foreach ($cmd in $commands) {
+    & $cmd
+    
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "`n[!] Error: Command failed with exit code $LASTEXITCODE" -ForegroundColor Red
+        Write-Host "Press any key to close this window..."
+        $null = [System.Console]::ReadKey($true)
+        exit $LASTEXITCODE
+    }
 }
