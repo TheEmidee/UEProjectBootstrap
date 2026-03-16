4c76e9767742e25ec2fa1669616c9e2887dac000
Michael Delva <michael@emidee.net>
2026-03-16 10:51:06 +0100

    Added a BuildMachine parameter in Setup.ps1 to avoid executing some actions

--- Files/Setup.ps1
+++ Files/Setup.ps1
@@ -1,13 +1,24 @@
+param (
+    [switch]$BuildMachine = $false
+ )
+
 try {
     Write-Host "Starting project setup..." -ForegroundColor Cyan
     Write-Host "Installing Python..." -ForegroundColor Cyan
     & "$bootstrapPythonFolder/install-python.ps1"
     Write-Host "Creating Python's virtual environment..." -ForegroundColor Cyan
     & "$bootstrapPythonFolder/setup-venv.ps1"
-    Write-Host "Installing pre-commit hooks..." -ForegroundColor Cyan
-    pre-commit install
-    Write-Host "Running python setup scripts..." -ForegroundColor Cyan
-    & "$bootstrapPythonFolder/run-setup-scripts.ps1"
+
+    if ($BuildMachine -eq $false) {
+        Write-Host "Installing pre-commit hooks..." -ForegroundColor Cyan
+        pre-commit install
+
+        Write-Host "Running python setup scripts..." -ForegroundColor Cyan
+        & "$bootstrapPythonFolder/run-setup-scripts.ps1"
+    } else {
+        Write-Host "Skipping pre-commit hooks installation and setup scripts execution on build machine." -ForegroundColor Yellow
+    }
+
     Write-Host "Checking engine installation..." -ForegroundColor Cyan
     & "$pythonScriptsFolder/ue-check-engine-installation.exe"
 }
